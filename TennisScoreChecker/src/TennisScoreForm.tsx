e8c884a051b15b01a5364eb3fad055a6258ed821
Swagathaur <aidan@ultimerse.com>
2021-03-14 09:05:10 +1100

    -Added ideal TennisScoreForm code

--- TennisScoreChecker/src/TennisScoreForm.tsx
+++ TennisScoreChecker/src/TennisScoreForm.tsx
@@ -3,8 +3,15 @@ import React from "react";
 function TennisScoreForm(props) {
     return (
         <div>
-
-        </div> 
+            <PlayerScoreForm 
+                player={props.playerX}
+                totalSets={props.totalSets}
+                class={props.winner === props.playerX ? 'winnerTable' : 'loserTable'} />
+            <PlayerScoreForm 
+                player={props.playerY}
+                totalSets={props.totalSets}
+                class={props.winner === props.playerY ? 'winnerTable' : 'loserTable'} />
+        </div>
     )
 }
 
