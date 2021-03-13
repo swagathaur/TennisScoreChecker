a9077a2961d5ab19c01c1070c580f9480aa1a223
Swagathaur <aidan@ultimerse.com>
2021-03-13 17:54:41 +1100

    -Added dummy prop data

--- TennisScoreChecker/src/__tests__/TennisScoreForm.spec.tsx
+++ TennisScoreChecker/src/__tests__/TennisScoreForm.spec.tsx
@@ -2,6 +2,7 @@ import "@testing-library/jest-dom"
 import "@testing-library/jest-dom/extend-expect"
 import React from 'react'
 import TennisScoreForm from '../TennisScoreForm'
+import Chance from 'chance'
 
 const { render } = require("@testing-library/react");
 
@@ -9,27 +10,59 @@ describe('<TennisScoreForm />', () => {
     describe('take match data and display it to a table', () => {
 
         let getByRole;
-        let props;
+        let props = GetDummyData();
 
         beforeEach(async () => {
             ({getByRole} = render(<TennisScoreForm props/>))
-        })
+        });
 
         it ("Displays both players' matchData", () => {
-            expect(getByRole('playerNameField', {name : 'playerX'})).toContain(props.playerX.firstname);
-            expect(getByRole('playerNameField', {name : 'playerY'})).toContain(props.playerY.firstname)
+            expect(getByRole('playerNameField', {name : props.playerX.firstName})).toContain(props.playerX.firstName);
+            expect(getByRole('playerNameField', {name : props.playerY.firstName})).toContain(props.playerY.firstName)
             
             for(let x = 0; x < props.totalSets; x++) {                
-                    expect(getByRole('gameRecordField', {name : 'gamesCount', gameSet : x, player : 'playerX'}))
-                        .toContain(props.playerX.gameSet[x].games);
-                    expect(getByRole('gameRecordField', {name : 'gamesCount', gameSet : x, player : 'playerY'}))
-                        .toContain(props.playerY.gameSet[x].games);
+                    expect(getByRole('gameRecordField', {name : 'gamesCount', gameSet : x, player : props.playerX.firstName}))
+                        .toContain(props.playerX.gameSet[x]);
+                    expect(getByRole('gameRecordField', {name : 'gamesCount', gameSet : x, player : props.playerY.firstName}))
+                        .toContain(props.playerY.gameSet[x]);
             }
                 
-            expect(getByRole('setRecordField', {name : 'setCount',  player : 'playerX'}))
-                        .toContain(props.playerX.matches);
-            expect(getByRole('setRecordField', {name : 'setCount',  player : 'playerY'}))
+            expect(getByRole('setRecordField', {name : 'setCount',  player : props.playerX.firstName}))
+                        .toContain(props.playerX.sets);
+            expect(getByRole('setRecordField', {name : 'setCount',  player : props.playerY.firstName}))
                         .toContain(props.playerY.sets);
         });
     });
 });
+
+//Create the dummy prop data for the test
+function GetDummyData(): any {
+    let props : any;
+    let chance = new Chance();
+
+    //Get random names
+    props.playerX = chance.name();
+    props.playerY = chance.name();
+
+    //Get total setCount
+    props.totalSets = chance.integer({min: 2, max: 3})
+    
+    //Set base set counts
+    props.playerX.sets = 0;
+    props.playerX.sets = 0;        
+    for (let i = 0; i < props.totalSets; i++) {
+        //Set random gameCount
+        props.playerX.gameSet[i] = chance.integer({min: 0, max: 6})
+        props.playerY.gameSet[i] = chance.integer({min: 0, max: 6})
+
+        //Set higher value to 6 to fake win set, will prefer Y if equal.
+        if (props.playerX.gameSet[i] > props.playerY.gameSet[i]) {
+            props.playerX.gameSet[i] = 6;
+            props.playerX.sets++;
+        }
+        else {
+            props.playerY.gameSet[i] = 6;
+            props.playerY.sets++;
+        }
+    }
+}
