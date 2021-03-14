4b0156832e85242eaf505eff8f93940145bdbd42
Swagathaur <aidan@ultimerse.com>
2021-03-15 09:59:59 +1100

    -Added some delicious styling

--- TennisScoreChecker/src/components/MatchScoreForm.tsx
+++ TennisScoreChecker/src/components/MatchScoreForm.tsx
@@ -3,21 +3,34 @@ import PlayerScoreForm from "./PlayerScoreForm";
 import styles from '../assets/tableStylings.module.css';
 
 function MatchScoreForm(props) {
-    return (
-        <div>
-            <h1>Match {props.id} </h1>
-            <table className={props.winner === props.playerX ? styles.winnerTable : styles.loserTable} >
-                <tbody>
-                    <PlayerScoreForm
-                        player={props.playerX}
-                        totalSets={props.totalSets} />
-                    <PlayerScoreForm
-                        player={props.playerY}
-                        totalSets={props.totalSets} />
-                </tbody>
-            </table>
-        </div>
-    )
+    let sets = [];
+    for (let i = 0; i < props.totalSets; i++)
+    {
+        sets.push(<td>Set {i + 1}</td>)
+    }
+
+        return (
+            <div>
+                <h1>Match {props.id} </h1>
+                <table className={props.winner === props.playerX ? styles.winnerTable : styles.loserTable} >
+                    <thead>
+                        <tr>
+                            <td> Players </td>
+                            {sets}
+                            <td>Match</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <PlayerScoreForm
+                            player={props.playerX}
+                            totalSets={props.totalSets} />
+                        <PlayerScoreForm
+                            player={props.playerY}
+                            totalSets={props.totalSets} />
+                    </tbody>
+                </table>
+            </div>
+        )
 }
 
 export default MatchScoreForm;
--- TennisScoreChecker/src/components/PlayerScoreForm.tsx
+++ TennisScoreChecker/src/components/PlayerScoreForm.tsx
@@ -4,15 +4,30 @@ function PlayerScoreForm(props) {
     //Make minimal game list elements
     let gameSet: any[] = [];
     for (let i = 0; i < props.totalSets; i++) {
-        gameSet.push(
-            <td key={i}> {props.player.gameSets[i]} </td>)
+        if (props.player.gameSets[i] == 6) {
+            gameSet.push(
+                <td key={i} style={{ backgroundColor: "greenyellow" }}>
+                    {props.player.gameSets[i]}
+                </td>)
+        }
+        else {
+            gameSet.push(
+                <td key={i} style={{ backgroundColor: "papayawhip" }}>
+                    {props.player.gameSets[i]}
+                </td>)
+        }
     }
+    let setDisplay;
+    if (props.player.sets === 2)
+        setDisplay = <td> {props.player.sets} </td>
+    else
+        setDisplay = <td > {props.player.sets} </td>
 
     return (
         <tr className={props.className} key={props.player.name}>
             <td key='name'> {props.player.name} </td>
             {gameSet}
-            <td > {props.player.sets}</td>
+            {setDisplay}
         </tr>
     )
 }
